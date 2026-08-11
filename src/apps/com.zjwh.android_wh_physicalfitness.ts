8f35da7e06653886ddffe109c6965d7cbffeab42
刘琦qqqqqqqq <17371703+liu-qis-endless-stream@user.noreply.gitee.com>
2026-08-11 12:21:38 +0800

    modified:   dist/CHANGELOG.md 	modified:   dist/README.md 	modified:   dist/gkd.json5 	modified:   dist/gkd.version.json5 	new file:   src/apps/com.zjwh.android_wh_physicalfitness.ts

---
+++ src/apps/com.zjwh.android_wh_physicalfitness.ts
@@ -0,0 +1,28 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.zjwh.android_wh_physicalfitness',
+  name: '运动世界',
+  groups: [
+    {
+      key: 1,
+      name: '广告-关闭按钮',
+      desc: '首页中下方广告与登录页广告右上角/左上角的关闭按钮',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: [
+        'com.zjwh.android_wh_physicalfitness.mvi.home.HomeActivity',
+        'com.zjwh.android_wh_physicalfitness.mvi.login.LoginActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.zjwh.android_wh_physicalfitness:id/ad_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/30921758', // HomeActivity
+            'https://i.gkd.li/i/30921773', // LoginActivity
+          ],
+        },
+      ],
+    },
+  ],
+});
