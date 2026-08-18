897ea819fed3877d3370d090d39b508b9c42d88f
刘琦qqqqqqqq <17371703+liu-qis-endless-stream@user.noreply.gitee.com>
2026-08-18 19:56:18 +0800

    添加了坚持退出

--- src/apps/com.tingfeng.tool.ts
+++ src/apps/com.tingfeng.tool.ts
@@ -7,12 +7,20 @@ export default defineGkdApp({
     {
       key: 1,
       name: '激励视频广告-关闭',
-      desc: '快手广告SDK的激励视频，点击"残忍离开"关闭广告',
+      desc: '快手广告SDK的激励视频，点击跳过后确认退出关闭广告',
       resetMatch: 'app',
       activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
       rules: [
         {
-          matches: '[text="残忍离开"]',
+          key: 0,
+          name: '点击跳过',
+          matches: '[text="跳过"]',
+        },
+        {
+          key: 1,
+          name: '确认退出',
+          preKeys: [0],
+          matches: '[text="坚持退出"]',
         },
       ],
     },
