d7d1028370f4f248edaedfca690de7c8cbfecdb7
llq098317-sketch <llq098317@gmail.com>
2026-08-18 13:14:29 +0800

    Refactor app configuration for com.tingfeng.tool

--- src/apps/com.tingfeng.tool.ts
+++ src/apps/com.tingfeng.tool.ts
@@ -1,20 +1,32 @@
-import { defineGkdApp } from '@gkd-kit/define';
-
-export default defineGkdApp({
-  id: 'com.tingfeng.tool',
-  name: '囧次元',
-  groups: [
+{
+  id: 2154816001,
+  name: '本地订阅-囧次元',
+  version: 1,
+  author: 'ql',
+  updateUrl: './gkd.version.json5',
+  categories: [],
+  globalGroups: [],
+  apps: [
     {
-      key: 1,
-      name: '激励视频广告-跳过',
-      desc: '快手广告SDK的激励视频，点击右上角跳过按钮',
-      resetMatch: 'app',
-      activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
-      rules: [
+      id: 'com.tingfeng.tool',
+      name: '囧次元',
+      groups: [
         {
-          matches: '[desc="skip_button"] > [text="跳过"]',
+          key: 1,
+          name: '激励视频广告-关闭',
+          desc: '快手广告SDK的激励视频页，点击"残忍离开"关闭广告',
+          resetMatch: 'app',
+          activityIds: [
+            'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          ],
+          rules: [
+            {
+              key: 11,
+              matches: '[text="残忍离开"]',
+            },
+          ],
         },
       ],
     },
   ],
-});
+}
