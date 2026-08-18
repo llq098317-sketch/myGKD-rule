018ad759549002eecbe2bf8c306f8db7305dd9fe
刘琦qqqqqqqq <17371703+liu-qis-endless-stream@user.noreply.gitee.com>
2026-08-18 19:47:48 +0800

    将判断跳过改成了判断残忍离开

--- src/apps/com.tingfeng.tool.ts
+++ src/apps/com.tingfeng.tool.ts
@@ -6,13 +6,13 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '激励视频广告-跳过',
-      desc: '快手广告SDK的激励视频，点击右上角跳过按钮',
+      name: '激励视频广告-关闭',
+      desc: '快手广告SDK的激励视频，点击"残忍离开"关闭广告',
       resetMatch: 'app',
       activityIds: ['com.kwad.sdk.api.proxy.app.KsRewardVideoActivity'],
       rules: [
         {
-          matches: '[desc="skip_button"] > [text="跳过"]',
+          matches: '[text="残忍离开"]',
         },
       ],
     },
